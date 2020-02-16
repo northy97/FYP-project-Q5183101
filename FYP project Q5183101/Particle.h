82a88ff5e8cf3b93c27f5be98e021803e816ab19
ASHLEY BROCKLESBY <ashleybrocklesby97@gmail.com>
2020-02-16 00:36:33 +0000

    updated particle
    a

--- FYP project Q5183101/Particle.h
+++ FYP project Q5183101/Particle.h
@@ -1,41 +1,57 @@
 #pragma once
-#include <iostream>
-#include <vector>
-
-
-
-using namespace std;
-
-struct Vector3
-{
-	float x, y, z;
-};
+#include<glm/glm.hpp>
+#include<gl/freeglut.h>
+using namespace glm;
 
 class Particle
 {
 
+private:
+	float Pressure;
+	float Density;
+	float Temperature;
+	float Mass;
+	vec3 Position;
+	vec3 Acceleration;
+	vec3 Velocity;
+
+
 public:
 
+	Particle()
+	{
+		vec3 Position = vec3(0.0f, 0.0f, 0.0f);
+		vec3 Acceleration = vec3(0.0f, 0.0f, 0.0f);
+		vec3 Velocity = vec3(0.0f, 0.0f, 0.0f);
+		Pressure = 0.0f;
+		Density = 0.0f;
+		Temperature = 0.0f;
+		Mass = 0.0f;
+
+	}
+
+
+	Particle(vec3 nPosition, vec3 nAcceleration, vec3 nVelocity, float nPressure, float nDensity, float nTemperature, float nMass)
+	{
+		Position = nPosition;
+		Acceleration = nAcceleration;
+		Velocity = nVelocity;
+		Pressure = nPressure;
+		Density = nDensity;
+		Temperature = nTemperature;
+		Mass = nMass;
+	}
+
+	void Display()
+	{
+		glutSolidSphere(1, 10, 10);
+	}
+
+
+	~Particle()
+	{
+
+	}
 	
-
-private:
-	vector<Particle*>neighbour;
-	float ID;
-	float mass;
-	float density;
-	float radius;
-	//float colour;
-	float pressure;
-	Vector3 lastPos;
-	Vector3 currentPos;
-	Vector3 lastVelo;
-	Vector3 currentVelo;
-	Vector3 lastAccel;
-	Vector3 currentAccel;
-	Vector3 surfaceNorm;
-
-
-
-
 };
 
