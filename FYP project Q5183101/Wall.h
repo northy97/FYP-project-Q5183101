85148d5536eb2be39103e66857ddc87f3c74018a
ASHLEY BROCKLESBY <ashleybrocklesby97@gmail.com>
2020-02-19 11:16:18 +0000

    adding walls and physics start
    a

---
+++ FYP project Q5183101/Wall.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <glm/glm.hpp>
+
+class Wall {
+public:
+	//Default constructor
+	Wall() {
+		center = glm::vec3(0.0f, 0.0f, 0.0f);
+		normal = glm::vec3(0.0f, 0.0f, 0.0f);
+		xlength = 0.0f;
+		ylength = 0.0f;
+	}
+
+	//Explicit constructor
+	Wall(glm::vec3 ncenter, glm::vec3 nnormal, float nxlength, float nylength) {
+		center = ncenter;
+		normal = nnormal;
+		xlength = nxlength;
+		ylength = nylength;
+	}
+
+	//Destructor
+	~Wall() {}
+
+	//private:
+	glm::vec3 normal;
+	glm::vec3 center;
+	float xlength;
+	float ylength;
+};
