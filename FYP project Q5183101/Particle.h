2f1802b55102b73216946d08fb664b9479fce9d3
ASHLEY BROCKLESBY <ashleybrocklesby97@gmail.com>
2020-02-20 00:53:24 +0000

    set up particle generation

--- FYP project Q5183101/Particle.h
+++ FYP project Q5183101/Particle.h
@@ -6,13 +6,12 @@ using namespace glm;
 class Particle
 {
 
-private:
 	float Pressure;
 	float Density;
-	float Temperature;
+	
 	float Mass;
 	vec3 Position;
-	vec3 Acceleration;
+
 	vec3 Velocity;
 
 
@@ -21,30 +20,29 @@ public:
 	Particle()
 	{
 		vec3 Position = vec3(0.0f, 0.0f, 0.0f);
-		vec3 Acceleration = vec3(0.0f, 0.0f, 0.0f);
 		vec3 Velocity = vec3(0.0f, 0.0f, 0.0f);
-		Pressure = 0.0f;
 		Density = 0.0f;
-		Temperature = 0.0f;
 		Mass = 0.0f;
 
 	}
 
 
-	Particle(vec3 nPosition, vec3 nAcceleration, vec3 nVelocity, float nPressure, float nDensity, float nTemperature, float nMass)
+	Particle(vec3 nPosition, vec3 nVelocity, float nDensity, float nMass)
 	{
 		Position = nPosition;
-		Acceleration = nAcceleration;
 		Velocity = nVelocity;
-		Pressure = nPressure;
 		Density = nDensity;
-		Temperature = nTemperature;
 		Mass = nMass;
 	}
 
 	void Display()
 	{
-		glutSolidSphere(1, 10, 10);
+		glLoadIdentity();
+		glTranslatef(Position.x, Position.y, -20.0);
+		glColor3f(0.9, 0.3, 0.2);
+		glutWireSphere(0.2, 20, 20);
+		
+	
 	}
 
 
--- FYP project Q5183101/main.cpp
+++ FYP project Q5183101/main.cpp
@@ -1,23 +1,91 @@
 #include <GLFW/glfw3.h>
 #include <GL/freeglut.h>
 #include <iostream>
-// set main loop and basic particle render with physics and timestep
+#include <vector>
+#include "Particle.h"
+
+
+// next step is to start implementing the physics system will look 2d until camera setup but will use 3d calculations
 GLfloat xRotated, yRotated, zRotated;
-GLdouble radius = 1;
+GLdouble Radius = 1;
+std::vector<Particle>particles;
+const int MaximumParticles = 2;
+bool generateParticles = false;
+bool toggleSim = false;
+const float radius = 0.1f;
+//Smoothing length
+const float smooth_length = 0.45;
+//The ideal density. This is the density of water
+const float rho0 = 10.0f;
+//The speed of sound in water
+const float c = 100.0f;
+const float epsilon = 0.01f;
+//timestep value
+const float dt = 0.033f;
+float total_time = 0.0f;
+
 
 
 void display(void);
 void reshape(int x, int y);
+void addParticle(vec3 Position, vec3 Velocity);
+void TimeStep();
+void initParticles();
+
 void idle(void)
 {
 
-	xRotated += 0.01;
-	//yRotated += 0.01;
-	zRotated += 0.01;
-	display();
+	int timems = glutGet(GLUT_ELAPSED_TIME);
+	
+	if (timems % 10 == 0) {
+	
+			
+		TimeStep();
+		
+	}
 }
 
+void initParticles()
+{
+	vec3 position(-4, 0, -20);
+	vec3 velocity(0, 0, 0 );
+	
+	
+	for (int y = 0; y < 5; y ++)
+	{
+		for (int x= 0; x < 5; x++)
+		{
+			addParticle(vec3(position.x + x, position.y+y,position.z), velocity);
+			std::cout << particles.size() << std::endl;
+		}
 
+	}
+
+
+}
+void addParticle(vec3 position, vec3 velocity)
+{
+	float density = rho0 + epsilon;
+	float mass = .1f * smooth_length * smooth_length * smooth_length * rho0;
+	Particle tempParticle(position, velocity,density,mass);
+	particles.push_back(tempParticle);
+}
+
+void TimeStep()
+{
+	std::cout << particles.size() << std::endl;
+
+	
+
+	//std::cout << "Grid updated" << std::endl;
+	//physics.update_density(particles);
+	//std::cout << "Density updated" << std::endl;
+	//physics.update_forces(particles, spheres);
+	//std::cout << "Forces updated" << std::endl;
+	//std::cout << "Pos/Vel updated" << std::endl;
+	//std::cout << "Step done" << std::endl;
+	total_time += dt;
+}
 
 int main(int argc, char** argv)
 {
@@ -27,7 +95,7 @@ int main(int argc, char** argv)
 	xRotated = yRotated = zRotated = 30.0;
 	xRotated = 43;
 	yRotated = 50;
-
+	initParticles();
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
 	glutIdleFunc(idle);
@@ -41,26 +109,11 @@ void display(void)
 	glMatrixMode(GL_MODELVIEW);
 	// clear the drawing buffer.
 	glClear(GL_COLOR_BUFFER_BIT);
-	// clear the identity matrix.
-	glLoadIdentity();
-	// traslate the draw by z = -4.0
-	// Note this when you decrease z like -8.0 the drawing will looks far , or smaller.
-	glTranslatef(0.0, 0.0, -20.0);
-	// Red color used to draw.
-	glColor3f(0.9, 0.3, 0.2);
-	// changing in transformation matrix.
-	// rotation about X axis
-	glRotatef(xRotated, 1.0, 0.0, 0.0);
-	// rotation about Y axis
-	glRotatef(yRotated, 0.0, 1.0, 0.0);
-	// rotation about Z axis
-	glRotatef(zRotated, 0.0, 0.0, 1.0);
-	// scaling transfomation 
-	glScalef(1.0, 1.0, 1.0);
-	// built-in (glut library) function , draw you a sphere.
-	glutWireSphere(radius, 20, 20);
-
-	// Flush buffers to screen
+	
+	for (int x = 0; x < particles.size(); x++)
+	{
+		particles[x].Display();
+	}
 
 	glFlush();
 	// sawp buffers called because we are using double buffering 
